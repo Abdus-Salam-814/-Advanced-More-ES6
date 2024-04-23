8c9bbf0c8b0be1e51fe733c968f5da2ae9631955
slam814 <abdussalam6111997@gmail.com>
2024-04-23 15:01:50 +0600

    i added some file

---
+++ reduce.js
@@ -0,0 +1,15 @@
+const numbers = [4, 5, 7, 1, 2, 66];
+const total = numbers.reduce((previous, current) => previous + current, 0);
+
+console.log(total);
+
+
+const multiply = numbers.reduce((p, c) => p*c, 1);
+
+console.log(multiply);
+
+console.log(4 * 5 * 7 * 1 * 2 * 66);
+
+const sum = numbers.reduce((p, c) => p + c, 0);
+
+console.log(sum);
